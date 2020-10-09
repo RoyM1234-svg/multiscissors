3f32a59db7a574f077d749b6485cc5b3f7f9ac4a
RoyM1234 <rmainfeld1@gmail.com>
2020-10-10 01:40:28 +0300

    Create network.py

---
+++ network.py
@@ -0,0 +1,37 @@
+import socket
+
+
+class Network():
+
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = "192.168.0.107"
+        self.port = 5555
+        self.addr = (self.host, self.port)
+        self.id = self.connect()
+        self.format = "utf-8"
+
+    def connect(self):
+        self.client.connect(self.addr)
+        return self.client.recv(2048).decode()
+
+    def getid(self):
+        return self.id
+
+    def send(self, data):
+        """
+        :param data: str
+        :return: str
+        """
+        try:
+            self.client.send(str.encode(data))
+            reply = self.client.recv(2048).decode()
+            print(reply)
+            return reply
+        except socket.error as e:
+            return str(e)
+
+# n = Network()
+# n.send(n.id + ":rock")
+# print(n.id)
+
