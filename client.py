362c797fbc3190bd76f1194fe85715e0af62e48d
RoyM1234 <rmainfeld1@gmail.com>
2020-10-10 01:40:30 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,50 +1,21 @@
-import socket
+from network import Network
 
 
-class Network():
-    # PORT = 5050
-    # SERVER = "192.168.0.107"
-    # FORMAT = "utf-8"
-    # ADDR = (SERVER,PORT)
-    # DISCONNECT_MESSAGE = "!DISCONNECT"
+def main():
+    n = Network()
+    id = n.getid()
+    p1 = input("choose your kind: ")
+    p2 = n.send(id + ":" + p1)
 
-    # client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    # client.connect(ADDR)
+    if p1 == p2:
+        print("its a tie")
+    elif p1 == 'paper' and p2 == 'scissors':
+        print ("player 2 won")
+    elif p1 == 'scissors' and p2 == 'rock':
+        print ("player 2 won")
+    elif p1 == 'rock' and p2 == 'paper':
+        print ("player 2 won")
+    else:
+        print ("player 1 won")
 
-    # def send(msg):
-    #     message = msg.encode(FORMAT)
-    #     msg_length = len(message)
-    #     send_length = str(msg_length).encode(FORMAT)
-    #     send_length += b' ' * (HEADER - len(send_length))
-    #     client.send(send_length)
-    #     client.send(message)
-    #     print(client.recv(2048).decode(FORMAT))
-    def __init__(self):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = "192.168.0.107"
-        self.port = 5555
-        self.addr = (self.host, self.port)
-        self.id = self.connect()
-        self.format = "utf-8"
-
-    def connect(self):
-        self.client.connect(self.addr)
-        return self.client.recv(2048).decode()
-
-    def send(self, data):
-        """
-        :param data: str
-        :return: str
-        """
-        try:
-            self.client.send(str.encode(data))
-            reply = self.client.recv(2048).decode()
-            print(reply)
-            return reply
-        except socket.error as e:
-            return str(e)
-
-n = Network()
-n.send(n.id + ":rock")
-print(n.id)
-
+main()
