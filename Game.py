9b7e7949947808b111aabe87518d0bd45a7de09f
RoyM1234 <rmainfeld1@gmail.com>
2020-10-11 17:23:31 +0300

    Create Game.py

---
+++ Game.py
@@ -0,0 +1,51 @@
+class Game:
+    def __init__(self, id):
+        self.p1Went = False
+        self.p2Went = False
+        self.ready = False
+        self.id = id
+        self.moves = [None, None]
+        self.wins = [0,0]
+        self.ties = 0
+
+    def get_player_move(self, p):
+        """
+        :param p: [0,1]
+        :return: Move
+        """
+        return self.moves[p]
+
+    def play(self, player, move):
+        self.moves[player] = move
+        if player == 0:
+            self.p1Went = True
+        else:
+            self.p2Went = True
+
+    def connected(self):
+        return self.ready
+
+    def bothWent(self):
+        return self.p1Went and self.p2Went
+
+    def winner(self):
+
+        p1 = self.moves[0]
+        p2 = self.moves[1]
+
+        if p1==p2:
+            winner = -1
+        elif p1 == 'paper' and p2 == 'scissors':
+            winner = 1
+        elif p1 == 'scissors' and p2 == 'rock':
+            winner = 1
+        elif p1 == 'rock' and p2 == 'paper':
+            winner = 1
+        else:
+            winner = 0
+
+        return winner
+
+    def resetWent(self):
+        self.p1Went = False
+        self.p2Went = False
