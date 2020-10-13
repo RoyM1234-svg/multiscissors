3d1f280e5fd211df62db69b4f06d44872d6ac344
RoyM1234 <rmainfeld1@gmail.com>
2020-10-13 13:39:54 +0300

    Update run_game.py

--- run_game.py
+++ run_game.py
@@ -1,11 +1,5 @@
-# Make a two-player Rock-Paper-Scissors game.
-# (Hint: Ask for player plays (using input), compare them, print out a message
-# of congratulations to the winner, and ask if the players want to start a new game)
-#
-# Rules:
-# - Rock beats scissors
-# - Scissors beats paper
-# - Paper beats rock
+from network import Network
+from Game import Game
 import numpy as np
 
 class player(object):
