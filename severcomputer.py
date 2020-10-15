5731e4828911b166641eba011b9893bde361fc2b
RoyM1234 <rmainfeld1@gmail.com>
2020-10-15 18:27:58 +0300

    Update severcomputer.py

--- severcomputer.py
+++ severcomputer.py
@@ -21,7 +21,7 @@ s.listen(2)
 print("Waiting for a connection")
 
 IdCount = 0
-movelist = [None,None]
+# movelist = [None,None]
 def threaded_client(conn,p,game):
     conn.send(str.encode(str(p)))
     # reply = ""
@@ -29,12 +29,17 @@ def threaded_client(conn,p,game):
         data = conn.recv(2048).decode()
         if data == "get":
             conn.sendall(pickle.dumps(game))
+        elif "get" in data:
+            data.replace("get","")
+            game.play(p,data)
+            conn.sendall(pickle.dumps(game))
         elif data:
             game.play(p,data)
-            movelist[p] = data
+            print(f"player{p+1} chose {data}")
+            # movelist[p] = data
             conn.sendall(pickle.dumps(game))
-        elif data == "dosconnect":
-            break
+        # elif data == "dosconnect":
+        #     break
     print ("Connection Closed")
     conn.close()
 
