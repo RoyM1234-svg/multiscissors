cba754fea534f15cbc1a2542bdb7393547129690
RoyM1234 <rmainfeld1@gmail.com>
2020-10-15 18:28:21 +0300

    Update try.py

--- try.py
+++ try.py
@@ -1,3 +1,14 @@
-x= "5"
-y = int(x)
-print (y+1)
+
+import threading
+def put_input():
+    global x 
+    move = input("choose your move: ")
+    x = move
+
+t1 = threading.Thread(target = put_input)
+t1.start()
+t1.join()
+for i in range(10):
+    print (x)
+
+
