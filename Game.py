0f0f68b93030252330d4d2e871c34484132304db
RoyM1234 <rmainfeld1@gmail.com>
2020-10-26 20:14:16 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -3,11 +3,10 @@ class Game:
         self.p1Went = False
         self.p2Went = False
         self.ready = False
-        # self.id = id
         self.moves = [None, None]
         self.wins = [0,0]
         self.ties = 0
-        # self.board = board()
+
 
     def get_player_move(self, p):
         """
